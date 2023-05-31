524eb06a2915186f1af0263fedf4db3daa0a2487
RobertoAG117 <125500565+RobertoAG117@users.noreply.github.com>
2023-05-31 13:32:31 -0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables{
+    public static void main(String[]args){
+     String nombre;
+     int edad;
+     double calificacion;
+     boolean aprobo;
+     //inicializa valor
+     nombre="Juan";
+     edad=30;
+     calificacion=8.5;
+     aprobo=true;
+     System.out.println(nombre);
+     System.out.println(edad);
+     System.out.println(calificacion);
+     System.out.println(aprobo);
+     System.out.println(nombre+" "+edad+" "+calificacion+" "+aprobo);
+ }
+}
