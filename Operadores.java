a74aff933c79684c4436b538ca490a7f80c8fca0
RobertoAG117 <125500565+RobertoAG117@users.noreply.github.com>
2023-06-01 13:14:30 -0600

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores{
+    public static void main (String[]args){
+        int entero= 1000000;
+        byte bytes= 120;
+        float precio= 9;
+        boolean verdadero= true;
+        long valordos= 4200000;
+        String nombre= "sam";
+        short valor= 2800;
+        double preciodos= 8.4549;
+        System.out.println(entero+" "+bytes+" "+precio+" "+verdadero+" "+valordos+" "+nombre+" "+valor+" "+preciodos);
+        //operadores aritmeticos
+        int suma= entero+5;
+        int resta= entero-500;
+        int multi= entero*2;
+        //int porcen= entero 5%
+        System.out.println(suma+" "+resta+" "+multi);
+    }
+}
