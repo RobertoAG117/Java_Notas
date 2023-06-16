2b2139abf88236d14ed9d62fdae3d38ee09f9b98
RobertoAG117 <125500565+RobertoAG117@users.noreply.github.com>
2023-06-16 13:53:06 -0600

    Add files via upload

---
+++ funciones.java
@@ -0,0 +1,14 @@
+public class funciones{
+    public static void main (String[]args){
+    int edad;
+    String nombre;
+    double calificacion;
+    boolean aprobo;
+    //inicializa valor
+    nombre = "Juan";
+    edad = 30;
+    calificacion = 8.5;
+    aprobo = true;
+    System.out.println(nombre+ edad+ calificacion+ aprobo);
+    }
+}
