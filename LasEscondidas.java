9f18a0ac7d4595614032a5ed9e78ace91c9bd66e
RobertoAG117 <125500565+RobertoAG117@users.noreply.github.com>
2023-06-16 13:21:29 -0600

    Add files via upload

---
+++ LasEscondidas.java
@@ -0,0 +1,17 @@
+public class LasEscondidas {
+
+public static void main(String[] args) {
+
+System.out.println("Juguemos a las escondidas.");
+
+System.out.print("Tres...");
+
+System.out.print("Dos...");
+
+System.out.println("Uno...");
+
+System.out.println("¡Listos o no, aquí voy!");
+
+}
+
+}
---
+++ Timeline.java
@@ -0,0 +1,14 @@
+public class Timeline {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Java!");
+        System.out.println("Naciste en 1995.");
+        //System.out.println("Sun Microsystems anunció el lanzamiento de Java en 1995.");
+        System.out.println("Fuiste creado por James Gosling.");
+        /*
+        System.out.println(“James Gosling es un ingeniero canadiense que
+        creó Java mientras trabajaba en Sun Microsystems.
+        ¡Su número favorito es la raíz cuadrada de 2!”);
+        */
+        System.out.println("¡Eres un lenguaje divertido!");
+    }
+}
