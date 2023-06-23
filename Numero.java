f53af475e9f27679e82e136ccaa260913ddddd18
RobertoAG117 <125500565+RobertoAG117@users.noreply.github.com>
2023-06-23 13:19:56 -0600

    Add files via upload

---
+++ Numero.java
@@ -0,0 +1,11 @@
+public class Numero {
+    public static void main(String[]args){
+        int nums[]= {12, 10, 4, 24, 5, 9};
+        
+        for(int j = 0; j < nums.length; j++){
+            System.out.println("La posicion en el arreglo es: " + j + "  y tiene el número: " + nums[j]);
+        }
+        System.out.println(" ");
+    }
+}
+
--- serie.java
+++ serie.java
@@ -4,20 +4,25 @@ public class serie {
         Scanner entrada = new Scanner(System.in);
         int elec= 0;
         int impar=1;
+
         System.out.println(" Bienvenido ");
         System.out.println("¿Que sererie quieres mostrar: ");
         System.out.println("Serie de impares= 1 o Serie Fibonacci= 2 ");
         System.out.println(" ");
+
         elec = entrada.nextInt();
         entrada.nextLine();
+
         if(elec == 1){
-            for(int i=1;i <= 15;i++){
-            System.out.println(impar+" ");
+            for(int i=1; i <= 15; i++){
+            System.out.println(impar + " ");
             impar += 2;
             }
         }else{
             int serie = 15, num1 = 0, num2 = 1, suma = 0;
-        System.out.println(num1);
+        
+            System.out.println(num1);
+
         for (int j = 1; j < serie; j++) {
             System.out.println(suma);
             suma = num1 + num2;
